bb09de1374fcc41fed32ae5cb78a77f44f5dd796
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-06-26 18:11:42 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 use std::fs;
-use std::env;
-#[derive(Debug)]
-
-struct Lexer {
-    contents: String,
-
-}
-
-impl Lexer {
-    pub fn new(contents: String) -> Self {
-        Self {
-            contents:contents
-        }
-    }
-}
+#[allow(unused)]
 
 fn main() {
-    let file = env::args().nth(1).unwrap();
-    let contents = fs::read_to_string(file).unwrap();
+    // read file
+    println!("In file {}", r"C:\Users\nyels\nyelx\examples\test.nys");
+    let contents = fs::read_to_string(r"C:\Users\nyels\nyelx\examples\test.nys")
+        .expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
+    let clear_contents = contents.replace(" ", "");
+    println!("{:?}", clear_contents);
+    let mut token_list: Vec<String> = Vec::new();
+    let mut mutable_char = String::from("");
+    for character in clear_contents.chars() { 
+        mutable_char.push(character);
+        match mutable_char.as_str() {
+            // ids
+            "log" => {
+                token_list.push(mutable_char.to_string());
+                mutable_char = String::new();
+            }
+            // ops
+            "+" | "-" | "/" | "*" | "%" | "(" | ")" => {
+                token_list.push(mutable_char.to_string());
+                mutable_char = String::new();
+            }
+            _ => print!("")
+        }
 
-    let lexer = Lexer::new(contents);
-
-    println!("{:?}", lexer)
+    }
+    println!("{:?}", token_list)
 }
-// cargo run -- ./examples/hello-world.nys
+
+pub fn match_string(character: String, vector: &mut Vec<String>){
+    match character.as_str() {
+        "log" => vector.push(character),
+        _ => print!("")
+    }
+}
