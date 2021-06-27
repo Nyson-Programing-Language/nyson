699e4aebddca2803ac8c5b0cd125f3221952057f
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-06-27 14:07:20 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,18 @@
+use std::env;
 use std::fs;
-#[allow(unused)]
 
 fn main() {
-    // read file
-    println!("In file {}", r"C:\Users\nyels\nyelx\examples\test.nys");
-    let contents = fs::read_to_string(r"C:\Users\nyels\nyelx\examples\test.nys")
-        .expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
-    let clear_contents = contents.replace(" ", "");
-    println!("{:?}", clear_contents);
-    let mut token_list: Vec<String> = Vec::new();
-    let mut mutable_char = String::from("");
-    for character in clear_contents.chars() { 
-        mutable_char.push(character);
-        match mutable_char.as_str() {
-            // ids
-            "log" => {
-                token_list.push(mutable_char.to_string());
-                mutable_char = String::new();
-            }
-            // ops
-            "+" | "-" | "/" | "*" | "%" | "(" | ")" => {
-                token_list.push(mutable_char.to_string());
-                mutable_char = String::new();
-            }
-            _ => print!("")
-        }
-
-    }
-    println!("{:?}", token_list)
-}
-
-pub fn match_string(character: String, vector: &mut Vec<String>){
-    match character.as_str() {
-        "log" => vector.push(character),
-        _ => print!("")
-    }
+    let maybe_file = env::args().nth(1);
+    let file = if let Some(f) = maybe_file {
+        f
+    } else {
+        panic!("Expected file");
+    };
+    let maybe_contents = fs::read_to_string(file);
+    let contents = if maybe_contents.is_ok() {
+        maybe_contents.unwrap()
+    } else {
+        panic!("Could not open file for reading.");
+    };
+    println!("{:?}", contents)
 }
