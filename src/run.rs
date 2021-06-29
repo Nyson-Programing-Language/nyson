eb3d0e1f638f82a88bfe9d060849b931397e0470
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-06-29 17:33:44 -0400

    declare to function

--- src/run.rs
+++ src/run.rs
@@ -1,6 +1,4 @@
 use std::ops::{Add, Sub, Mul, Div};
-#[allow(unused_variables)]
-#[allow(unused_mut)]
 
 pub fn run(contents: Vec<String>) {
     let mut memory_names: Vec<String> = Vec::new();
@@ -19,148 +17,7 @@ pub fn run(contents: Vec<String>) {
                 println!("{}", math(x, contents.clone()));
             }
             else if contents[x] == "dec" {
-                let memory_names_save = memory_names.clone();
-                let memory_types_save = memory_types.clone();
-                let memory_values_save = memory_values.clone();
-                let move_up = 2;
-                if contents[x+move_up] == "int" {
-                    memory_types.push(String::from("int"));
-                } else if contents[x+move_up] == "str"  {
-                    memory_types.push(String::from("str"));
-                }
-                memory_names.push(String::from(contents[x+move_up+move_up].clone()));
-                let move_up_up = 1;
-                if contents[x+move_up+move_up+1] == ":" {
-                    let move_up_up = 1;
-                }
-                if contents[x+move_up+move_up+1] == " " {
-                    let move_up_up = 2;
-                }
-                if contents[x+move_up+move_up+move_up_up] == " " {
-                    let mut value = String::new();
-                    let mut move_final = 2;
-                    if contents[x+move_up+move_up+move_up_up+2] == " " {
-                        let mut move_final = 2;
-                    } else {
-                        let mut move_final = 1;
-                    }
-                    let mut n = 0;
-                    let mut quote = 0;
-                    loop {
-                        if contents[x+move_up+move_up+move_up_up+move_final] == ";" {
-                            break;
-                        }
-                        else {
-                            if contents[x+move_up+move_up+move_up_up+move_final] == "\"" || contents[x+move_up+move_up+move_up_up+move_final] == "\'" || contents[x+move_up+move_up+move_up_up+move_final] == r"\`" {
-                                quote = quote + 1;
-                            }
-                            else {
-                                if contents[x+move_up+move_up+move_up_up+move_final] == "math" {
-                                    value.push_str(math(x+move_up+move_up+move_up_up+move_final, contents.clone()).to_string().as_str());
-                                    n = 1;
-                                }
-                                else {
-                                    if n == 0 {
-                                        if quote%2 == 1 {
-                                            value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
-                                        }
-                                        else {
-                                            let mut position = memory_names_save.len();
-                                            let mut skip = false;
-                                            for pos in 0..memory_names_save.len() {
-                                                if skip == false {
-                                                    if memory_names_save[pos].to_string() == contents[x+move_up+move_up+move_up_up+move_final].to_string() {
-                                                        position = pos;
-                                                        skip = true;
-                                                    }
-                                                }
-                                            }
-                                            if position != memory_names_save.len() {
-                                                value.push_str(memory_values_save[position].to_string().as_str());
-                                            }
-                                            else {
-                                                value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
-                                            }
-                                        }
-                                    }
-                                }
-                                if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == "(" {
-                                    n = n + 1
-                                }
-                                else if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == ")" {
-                                    n = n - 1;
-                                    if n == 1 {
-                                        n = 0;
-                                    }
-                                }
-                            }
-                        }
-                        move_final = move_final+1;
-                    }
-                    memory_values.push(value);
-                } else {
-                    let mut value = String::new();
-                    let mut move_final = 2;
-                    if contents[x+move_up+move_up+move_up_up+2] == " " {
-                        let mut move_final = 2;
-                    } else {
-                        let mut move_final = 1;
-                    }
-                    let mut n = 0;
-                    let mut quote = 0;
-                    loop {
-                        if contents[x+move_up+move_up+move_up_up+move_final] == ";" {
-                            break;
-                        }
-                        else {
-                            if contents[x+move_up+move_up+move_up_up+move_final] == "\"" || contents[x+move_up+move_up+move_up_up+move_final] == "\'" || contents[x+move_up+move_up+move_up_up+move_final] == r"\`" {
-                                quote = quote + 1;
-                            }
-                            else {
-                                if contents[x+move_up+move_up+move_up_up+move_final] == "math" {
-                                    value.push_str(math(x+move_up+move_up+move_up_up+move_final, contents.clone()).to_string().as_str());
-                                    n = 1;
-                                }
-                                else {
-                                    if n == 0 {
-                                        if quote%2 == 1 {
-                                            value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
-                                        }
-                                        else {
-                                            let mut position = memory_names_save.len();
-                                            let mut skip = false;
-                                            for pos in 0..memory_names_save.len() {
-                                                if skip == false {
-                                                    if memory_names_save[pos].to_string() == contents[x+move_up+move_up+move_up_up+move_final].to_string() {
-                                                        position = pos;
-                                                        skip = true;
-                                                    }
-                                                }
-                                            }
-                                            if position != memory_names_save.len() {
-                                                value.push_str(memory_values_save[position].to_string().as_str());
-                                            }
-                                            else {
-                                                value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
-                                            }
-                                        }
-                                    }
-                                }
-                                if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == "(" {
-                                    n = n + 1
-                                }
-                                else if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == ")" {
-                                    n = n - 1;
-                                    if n == 1 {
-                                        n = 0;
-                                    }
-                                }
-                            }
-                        }
-                        move_final = move_final+1;
-                    }
-                    memory_values.push(value);
-                }
+                declare(memory_names, memory_types, memory_values);
             }
         }
     }
@@ -349,4 +206,150 @@ pub fn math(x:usize, contents: Vec<String>) -> i32 {
         }
     }
     return vec[0].parse().unwrap();
+}
+
+pub fn declare(memory_names: Vec<String>, memory_types: Vec<String>, memory_values: Vec<String>) {
+    let memory_names_save = memory_names.clone();
+    let memory_types_save = memory_types.clone();
+    let memory_values_save = memory_values.clone();
+    let move_up = 2;
+    if contents[x+move_up] == "int" {
+        memory_types.push(String::from("int"));
+    } else if contents[x+move_up] == "str"  {
+        memory_types.push(String::from("str"));
+    }
+    move_up = move_up + 2;
+    memory_names.push(String::from(contents[x+move_up].clone()));
+    let move_up_up = 1;
+    if contents[x+move_up+1] == ":" {
+        move_up = move_up + 1;
+    }
+    if contents[x+move_up+1] == " " {
+        move_up = move_up + 2;
+    }
+    if contents[x+move_up+move_up+move_up_up] == " " {
+        let mut value = String::new();
+        let mut move_final = 2;
+        if contents[x+move_up+move_up+move_up_up+2] == " " {
+            let mut move_final = 2;
+        } else {
+            let mut move_final = 1;
+        }
+        let mut n = 0;
+        let mut quote = 0;
+        loop {
+            if contents[x+move_up+move_up+move_up_up+move_final] == ";" {
+                break;
+            }
+            else {
+                if contents[x+move_up+move_up+move_up_up+move_final] == "\"" || contents[x+move_up+move_up+move_up_up+move_final] == "\'" || contents[x+move_up+move_up+move_up_up+move_final] == r"\`" {
+                    quote = quote + 1;
+                }
+                else {
+                    if contents[x+move_up+move_up+move_up_up+move_final] == "math" {
+                        value.push_str(math(x+move_up+move_up+move_up_up+move_final, contents.clone()).to_string().as_str());
+                        n = 1;
+                    }
+                    else {
+                        if n == 0 {
+                            if quote%2 == 1 {
+                                value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
+                            }
+                            else {
+                                let mut position = memory_names_save.len();
+                                let mut skip = false;
+                                for pos in 0..memory_names_save.len() {
+                                    if skip == false {
+                                        if memory_names_save[pos].to_string() == contents[x+move_up+move_up+move_up_up+move_final].to_string() {
+                                            position = pos;
+                                            skip = true;
+                                        }
+                                    }
+                                }
+                                if position != memory_names_save.len() {
+                                    value.push_str(memory_values_save[position].to_string().as_str());
+                                }
+                                else {
+                                    value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
+                                }
+                            }
+                        }
+                    }
+                    if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == "(" {
+                        n = n + 1
+                    }
+                    else if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == ")" {
+                        n = n - 1;
+                        if n == 1 {
+                            n = 0;
+                        }
+                    }
+                }
+            }
+            move_final = move_final+1;
+        }
+        memory_values.push(value);
+    } else {
+        let mut value = String::new();
+        let mut move_final = 2;
+        if contents[x+move_up+move_up+move_up_up+2] == " " {
+            let mut move_final = 2;
+        } else {
+            let mut move_final = 1;
+        }
+        let mut n = 0;
+        let mut quote = 0;
+        loop {
+            if contents[x+move_up+move_up+move_up_up+move_final] == ";" {
+                break;
+            }
+            else {
+                if contents[x+move_up+move_up+move_up_up+move_final] == "\"" || contents[x+move_up+move_up+move_up_up+move_final] == "\'" || contents[x+move_up+move_up+move_up_up+move_final] == r"\`" {
+                    quote = quote + 1;
+                }
+                else {
+                    if contents[x+move_up+move_up+move_up_up+move_final] == "math" {
+                        value.push_str(math(x+move_up+move_up+move_up_up+move_final, contents.clone()).to_string().as_str());
+                        n = 1;
+                    }
+                    else {
+                        if n == 0 {
+                            if quote%2 == 1 {
+                                value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
+                            }
+                            else {
+                                let mut position = memory_names_save.len();
+                                let mut skip = false;
+                                for pos in 0..memory_names_save.len() {
+                                    if skip == false {
+                                        if memory_names_save[pos].to_string() == contents[x+move_up+move_up+move_up_up+move_final].to_string() {
+                                            position = pos;
+                                            skip = true;
+                                        }
+                                    }
+                                }
+                                if position != memory_names_save.len() {
+                                    value.push_str(memory_values_save[position].to_string().as_str());
+                                }
+                                else {
+                                    value.push_str(contents[x+move_up+move_up+move_up_up+move_final].as_str());
+                                }
+                            }
+                        }
+                    }
+                    if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == "(" {
+                        n = n + 1
+                    }
+                    else if n >= 1 && contents[x+move_up+move_up+move_up_up+move_final] == ")" {
+                        n = n - 1;
+                        if n == 1 {
+                            n = 0;
+                        }
+                    }
+                }
+            }
+            move_final = move_final+1;
+        }
+        memory_values.push(value);
+    }
 }
