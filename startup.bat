f45eac31300ce244436632b027b3cda40b7e6b55
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-07-01 11:11:00 -0400

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -3,6 +3,6 @@ cd C:\Users\%username%
 git clone https://github.com/Nyelsonon/nyson-programming-language.git
 cd nyson-programming-language
 cargo build --release
-SET "PATH=%PATH%;C:\nyson-programming-language\target\release"
-setx /M PATH "%PATH%;C:\nyson-programming-language\target\release"
+SET "PATH=%PATH%;C:\Users\%username%\nyson-programming-language\target\release"
+setx /M PATH "%PATH%;C:\Users\%username%\nyson-programming-language\target\release"
 cd %curpath%
