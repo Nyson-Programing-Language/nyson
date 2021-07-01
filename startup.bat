cde2cdd14626f9f48b43a273ac77192fd7551d5d
Arthur Melton <amtitan@icloud.com>
2021-06-30 21:57:46 -0400

    when you restart your computer it will still have the programing language

--- startup.bat
+++ startup.bat
@@ -4,4 +4,5 @@ git clone https://github.com/Nyelsonon/nyson-programming-language.git
 cd nyson-programming-language
 cargo build --release
 SET "PATH=%PATH%;C:\nyson-programming-language\target\release"
+setx /M PATH "%PATH%;C:\nyson-programming-language\target\release"
 cd %curpath%
