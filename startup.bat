bcf931722be4b117e8011c2f2023321721d062fa
Arthur Melton <amtitan@icloud.com>
2021-06-30 21:42:01 -0400

    startup.bat is better now

--- startup.bat
+++ startup.bat
@@ -1,5 +1,7 @@
+set "curpath=%cd%"
 cd C:\
 git clone https://github.com/Nyelsonon/nyson-programming-language.git
 cd nyson-programming-language
 cargo build --release
-SET PATH=%PATH%;C:\nyson-programming-language\target\release
+SET "PATH=%PATH%;C:\nyson-programming-language\target\release"
+cd %curpath%
